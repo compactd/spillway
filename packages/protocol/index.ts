1b96858eb1ca8c2f2e34d6e1d8fbb618d45e1c86
vinz243 <vinz243@gmail.com>
2018-09-25 14:29:12 +0200

    feat(protocol): add index file

---
+++ packages/protocol/index.ts
@@ -0,0 +1,4 @@
+export { default as DownstreamWire } from './src/DownstreamWire';
+export { default as UpstreamWire } from './src/UpstreamWire';
+export { default as WirePool } from './src/WirePool';
+export * from './src/definitions';
