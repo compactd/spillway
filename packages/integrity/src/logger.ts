00b9343d471daa922c2068794cc5091cfa6ddd4e
vinz243 <vinz243@gmail.com>
2018-10-02 19:25:38 +0200

    feat(integrity): fire callback once downloaded and verified

---
+++ packages/integrity/src/logger.ts
@@ -0,0 +1,6 @@
+import * as Debug from 'debug';
+
+export const log = Debug('integrity');
+export const warn = Debug('integrity:warn');
+export const err = Debug('integrity:err');
+export const trace = Debug('integrity:trace');
--- packages/integrity/src/withIntegrity.ts
+++ packages/integrity/src/withIntegrity.ts
@@ -1,3 +1,5 @@
+import { log, warn } from './logger';
+
 export interface IStorage {
   put(index: number, content: Buffer): Promise<void>;
   get(index: number): Promise<Buffer>;
@@ -9,18 +11,60 @@ export default function withIntegrity<
   T extends { new (...args: any[]): IStorage }
 >(Base: T, checkIntegrity: (hash: string, content: Buffer) => Promise<void>) {
   return class WrappedBase extends Base {
-    private pieces: string[] = [];
+    private pieces!: string[];
+    private validPieces = new Set();
+    private downloadCallbacks: (() => void)[] = [];
+
     setPieces(pieces: string[]) {
+      log('loading %d pieces', pieces.length);
+
       this.pieces = pieces;
+      this.validPieces.clear();
+
+      Promise.all(
+        pieces.map(async (_, index) => {
+          try {
+            await this.get(index);
+            this.validPieces.add(index);
+          } catch {}
+        }),
+      ).then(() => {
+        log('piece %d is valid', this.validPieces.size);
+        this.fireIfDone();
+      });
     }
+
     async put(index: number, content: Buffer) {
       await checkIntegrity(this.pieces[index], content);
-      super.put(index, content);
+
+      await super.put(index, content);
+
+      this.validPieces.add(index);
+      this.fireIfDone();
     }
+
     async get(index: number) {
       const content = await super.get(index);
+
       await checkIntegrity(this.pieces[index], content);
+
+      this.validPieces.add(index);
+      this.fireIfDone();
+
       return content;
+    }
+
+    waitUntilDownloaded() {
+      return new Promise(resolve => {
+        this.downloadCallbacks.push(resolve);
+      });
+    }
+
+    private fireIfDone() {
+      if (this.validPieces.size === this.pieces.length) {
+        this.downloadCallbacks.forEach(cb => cb());
+        this.downloadCallbacks = [];
+      }
     }
   };
 }
