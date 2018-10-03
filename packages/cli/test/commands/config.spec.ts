5e77ae59e93e89a4dda8f801c89da4380dcbbc0c
vinz243 <vinz243@gmail.com>
2018-10-03 17:59:27 +0200

    test(cli): increase timeout inside test

--- packages/cli/test/commands/config.spec.ts
+++ packages/cli/test/commands/config.spec.ts
@@ -11,6 +11,7 @@ describe('config', async () => {
   afterEach(() => {});
 
   it('should write 512 random bytes to secret if no value', async () => {
+    jest.setTimeout(10000);
     const writeFile = jest.fn((_, __, ___, cb) => process.nextTick(cb));
     const readFile = jest.fn((_, cb) => process.nextTick(cb));
     const mkdir = jest.fn((_, cb) => process.nextTick(cb));
